b4555c0ec8e11aa0fd35dda0ee0fc4033f343ffc
KarlosElyas <karloselyas2@gmail.com>
2023-07-20 17:57:58 -0300

    Lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+@IonicPage() // isso fara que o nome dessa classe seja obtido como String
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html' // html que esta sendo controlado por este Controller
 })
 export class HomePage {
 
