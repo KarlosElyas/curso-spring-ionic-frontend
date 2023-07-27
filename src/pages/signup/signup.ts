b0c8773375f39949726b0618f6ccd8dd1fba7523
KarlosElyas <karloselyas2@gmail.com>
2023-07-27 09:16:56 -0300

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
+import { EstadoDTO } from '../../models/estado.dto';
+import { CidadeDTO } from '../../models/cidade.dto';
 
 @IonicPage()
 @Component({
@@ -10,11 +14,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   formGroup: FormGroup;
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public formBuilder: FormBuilder
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService
     ) {
       this.formGroup = this.formBuilder.group({
         nome: ['Joaquim Barbosa', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -35,6 +43,29 @@ export class SignupPage {
         estadoId : [null, [Validators.required]],
         cidadeId : [null, [Validators.required]]
       })
+  }
+  
+  ionViewDidLoad() { // assim que carregar já busca no banco as cidade e deixa a primeira SELECIONADA
+    this.estadoService.findAll().subscribe(
+      response => {
+        this.estados = response;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id); // deixa o estado 0 já selecionado
+        this.updateCidades();
+      },
+      error => {}
+    );
+  }
+
+  // acessar o banco todas vez que muda o estado SOBRECARREGA O BACKEND
+  updateCidades() { 
+    let estado_id = this.formGroup.value.estadoId; // pega o estado selecionado no html
+    this.cidadeService.findAll(estado_id).subscribe(
+      response => {
+        this.cidades = response;
+        this.formGroup.controls.cidadeId.setValue(null); // pra desmarcar a cidade que estiver selecionada
+      },
+      error => {}
+    );
   }
 
   signupUser() {
